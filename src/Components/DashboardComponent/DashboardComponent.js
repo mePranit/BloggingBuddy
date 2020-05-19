8cbcb2df8d1b77849fdc3c7c3dba6f0fd37dd6d8
pranit poudel <logicalpranitz18@gmail.com>
2020-05-19 19:19:10 +0545

    popup for posting blog designed and implemented

--- src/Components/DashboardComponent/DashboardComponent.js
+++ src/Components/DashboardComponent/DashboardComponent.js
@@ -50,37 +50,38 @@ class DashboardComponent extends React.Component {
 
     openPopupbox() {
         const content = (
-            <div class="container">
+            <div class="container" style={{ backgroundColor: "#F5F8FD" }}>
 
-                <header class="jumbotron my-4">
+                <form >
+                    {/* <p><b>Post new blog</b></p> */}
+                    <label for="username"><b>Category</b></label>
+                    <input type="text" placeholder="Enter Category" name="username" required />
 
-                    <div class="container" >
-                        <form >
-                            <textarea style={{ width: "auto" }} textarea id="w3mission" rows="5" cols="50">
+                    <label><b>Content</b></label>
+                    <textarea class="form-control" rows="5" cols="100">
 
-                            </textarea>
+                    </textarea>
 
-                            <div class="row">
+                    <div class="row">
 
-                                <div class="col-lg-4 mb-4" >
-                                    <button  class="btn btn-success" type="submit" value="signup">Upload</button>
-                                </div>
-                                <div class="col-lg-4 mb-4" >
-                                    <button class="btn btn-danger" type="submit" value="signup">Cancel</button>
-                                </div>
-                                
-                            </div>
-                            </form>
-                    </div>
-                </header>
+                        <div class="col-lg-4 mb-4" >
+                            <button class="btn btn-success" type="submit" value="signup">Upload</button>
+                        </div>
+                        <div class="col-lg-4 mb-4" >
+                            <button class="btn btn-danger" type="submit" value="signup">Cancel</button>
+                        </div>
 
                     </div>
+                </form>
+
+            </div>
         )
 
         PopupboxManager.open({
-                        content,
-                        config: {
-                        fadeIn: true,
+            content,
+            config: {
+                fadeIn: true,
+
                 fadeInSpeed: 500,
 
             }
@@ -100,91 +101,93 @@ class DashboardComponent extends React.Component {
             <body   >
 
 
-                        {/* <Popup trigger={<button> Trigger</button>} position="right center">
+                {/* <Popup trigger={<button> Trigger</button>} position="right center">
                     <div>Popup content here !!</div>
                 </Popup> */}
-                        <Container >
-                            <PopupboxContainer />
-                        </Container>
 
-                        <Container>
-                            <Button tooltip="Post new blog" icon="fas fa-plus" onClick={this.openPopupbox} >
-                                Add
+
+                <Container>
+                    <Button tooltip="Post new blog" icon="fas fa-plus" onClick={this.openPopupbox} >
+                        Add
                     </Button>
-
-                        </Container>
-
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli </h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
-
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
-
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                </Container>
+                
+                <Container >
+                   <PopupboxContainer />
+                </Container>
 
 
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                <header class="jumbotron my-4">
+                    <form >
+                        <h3>Virat Kohli </h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
+
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
+
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
 
 
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
 
 
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
+
+
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
 
 
 
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                    {/* </header> */}
 
 
-                        <header class="jumbotron my-4">
-                            <form >
-                                <h3>Virat Kohli</h3>
-                                <p><b>Sports , 5 AM</b></p>
-                                <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
-                            </form>
-                        </header>
+                    {/* <header class="jumbotron my-4"> */}
+                    <form >
+                        <h3>Virat Kohli</h3>
+                        <p><b>Sports , 5 AM</b></p>
+                        <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument. </p>
+                    </form>
+                </header>
 
-                    </body>
+            </body>
         )
     }
 }
